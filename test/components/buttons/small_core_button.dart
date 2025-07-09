64728ef74b27663d0426dad7be496fcc5bfa8abb
nyel <yhiamdan@gmail.com>
2025-07-09 20:37:59 +0000

    added pressed state

--- test/components/buttons/small_core_button.dart
+++ test/components/buttons/small_core_button.dart
@@ -8,108 +8,133 @@ void main() {
     await loadAppFonts();
   });
 
-  testGoldens('CoreButton Small - Narrow View - With Pressed State (All Variants)', (tester) async {
-    final builder = GoldenBuilder.grid(columns: 2,widthToHeightRatio: 2)
-      ..addScenario('Primary', CoreButton(
-        label: 'Primary',
-        onPressed: () {},
-        variant: CoreButtonVariant.primary,
-        size: CoreButtonSize.small,
-      ))
-      ..addScenario('Primary + Icon', CoreButton(
-        label: 'Primary',
-        onPressed: () {},
-        variant: CoreButtonVariant.primary,
-        size: CoreButtonSize.small,
-        icon: const CoreIconWidget(icon: CoreIcons.arrowLeft, color: CoreTextColors.inverse),
-      ))
-      ..addScenario('Primary Disabled + Icon', CoreButton(
-        label: 'Primary',
-        onPressed: () {},
-        isDisabled: true,
-        variant: CoreButtonVariant.primary,
-        size: CoreButtonSize.small,
-        icon: const CoreIconWidget(icon: CoreIcons.arrowLeft, color: CoreTextColors.body),
-      ))
-      ..addScenario('Primary Focused', CoreButton(
-        label: 'Primary',
-        onPressed: () {},
-        variant: CoreButtonVariant.primary,
-        size: CoreButtonSize.small,
-        autofocus: true,
-        icon: const CoreIconWidget(icon: CoreIcons.arrowLeft, color: CoreTextColors.inverse),
-      ))
-      ..addScenario('Primary Pressed', CoreButton(
-        key: const Key('pressed_primary'),
-        label: 'Primary',
-        onPressed: () {},
-        variant: CoreButtonVariant.primary,
-        size: CoreButtonSize.small,
-        icon: const CoreIconWidget(icon: CoreIcons.arrowLeft, color: CoreTextColors.inverse),
-      ))
-
-      ..addScenario('Secondary', CoreButton(
-        label: 'Secondary',
-        onPressed: () {},
-        variant: CoreButtonVariant.secondary,
-        size: CoreButtonSize.small,
-      ))
-      ..addScenario('Secondary + Icon', CoreButton(
-        label: 'Secondary',
-        onPressed: () {},
-        variant: CoreButtonVariant.secondary,
-        size: CoreButtonSize.small,
-        icon: const CoreIconWidget(icon: CoreIcons.arrowLeft, color: CoreButtonColors.surface),
-      ))
-      ..addScenario('Secondary Disabled + Icon', CoreButton(
-        label: 'Secondary',
-        onPressed: () {},
-        isDisabled: true,
-        variant: CoreButtonVariant.secondary,
-        size: CoreButtonSize.small,
-        icon: const CoreIconWidget(icon: CoreIcons.arrowLeft, color: CoreTextColors.disable),
-      ))
-      ..addScenario('Secondary Focused', CoreButton(
-        label: 'Secondary',
-        onPressed: () {},
-        variant: CoreButtonVariant.secondary,
-        size: CoreButtonSize.small,
-        autofocus: true,
-        icon: const CoreIconWidget(icon: CoreIcons.arrowLeft, color: CoreButtonColors.hover),
-      ))
-      ..addScenario('Secondary Pressed', CoreButton(
-        key: const Key('pressed_secondary'),
-        label: 'Secondary',
-        onPressed: () {},
-        variant: CoreButtonVariant.secondary,
-        size: CoreButtonSize.small,
-        icon: const CoreIconWidget(icon: CoreIcons.arrowLeft, color: CoreButtonColors.press),
-      ));
+  testGoldens(
+      'CoreButton Small - Narrow View - With Pressed State (All Variants)',
+      (tester) async {
+    final builder = GoldenBuilder.grid(columns: 2, widthToHeightRatio: 2)
+      ..addScenario(
+          'Primary',
+          CoreButton(
+            label: 'Primary',
+            onPressed: () {},
+            variant: CoreButtonVariant.primary,
+            size: CoreButtonSize.small,
+          ))
+      ..addScenario(
+          'Primary + Icon',
+          CoreButton(
+            label: 'Primary',
+            onPressed: () {},
+            variant: CoreButtonVariant.primary,
+            size: CoreButtonSize.small,
+            icon: const CoreIconWidget(
+                icon: CoreIcons.arrowLeft, color: CoreTextColors.inverse),
+          ))
+      ..addScenario(
+          'Primary Disabled + Icon',
+          CoreButton(
+            label: 'Primary',
+            onPressed: () {},
+            isDisabled: true,
+            variant: CoreButtonVariant.primary,
+            size: CoreButtonSize.small,
+            icon: const CoreIconWidget(
+                icon: CoreIcons.arrowLeft, color: CoreTextColors.body),
+          ))
+      ..addScenario(
+          'Primary Focused',
+          CoreButton(
+            label: 'Primary',
+            onPressed: () {},
+            variant: CoreButtonVariant.primary,
+            size: CoreButtonSize.small,
+            autofocus: true,
+            icon: const CoreIconWidget(
+                icon: CoreIcons.arrowLeft, color: CoreTextColors.inverse),
+          ))
+      ..addScenario(
+          'Primary Pressed',
+          CoreButton(
+            key: const Key('pressed_primary'),
+            label: 'Primary',
+            onPressed: () {},
+            variant: CoreButtonVariant.primary,
+            size: CoreButtonSize.small,
+            icon: const CoreIconWidget(
+                icon: CoreIcons.arrowLeft, color: CoreTextColors.inverse),
+          ))
+      ..addScenario(
+          'Secondary',
+          CoreButton(
+            label: 'Secondary',
+            onPressed: () {},
+            variant: CoreButtonVariant.secondary,
+            size: CoreButtonSize.small,
+          ))
+      ..addScenario(
+          'Secondary + Icon',
+          CoreButton(
+            label: 'Secondary',
+            onPressed: () {},
+            variant: CoreButtonVariant.secondary,
+            size: CoreButtonSize.small,
+            icon: const CoreIconWidget(
+                icon: CoreIcons.arrowLeft, color: CoreButtonColors.surface),
+          ))
+      ..addScenario(
+          'Secondary Disabled + Icon',
+          CoreButton(
+            label: 'Secondary',
+            onPressed: () {},
+            isDisabled: true,
+            variant: CoreButtonVariant.secondary,
+            size: CoreButtonSize.small,
+            icon: const CoreIconWidget(
+                icon: CoreIcons.arrowLeft, color: CoreTextColors.disable),
+          ))
+      ..addScenario(
+          'Secondary Focused',
+          CoreButton(
+            label: 'Secondary',
+            onPressed: () {},
+            variant: CoreButtonVariant.secondary,
+            size: CoreButtonSize.small,
+            autofocus: true,
+            icon: const CoreIconWidget(
+                icon: CoreIcons.arrowLeft, color: CoreButtonColors.hover),
+          ))
+      ..addScenario(
+          'Secondary Pressed',
+          CoreButton(
+            key: const Key('pressed_secondary'),
+            label: 'Secondary',
+            onPressed: () {},
+            variant: CoreButtonVariant.secondary,
+            size: CoreButtonSize.small,
+            icon: const CoreIconWidget(
+                icon: CoreIcons.arrowLeft, color: CoreButtonColors.press),
+          ));
 
     await tester.pumpWidgetBuilder(
       Container(
         color: CoreBackgroundColors.pageBackground,
         child: builder.build(),
       ),
-      surfaceSize: const Size(500, 800), // Tall enough for 15 buttons
+      surfaceSize: const Size(500, 800),
     );
-    final pressedKeys = [
-      'pressed_primary',
-      'pressed_secondary'
-    ];
+    final primaryFinder = find.byKey(const Key('pressed_primary'));
+    final primaryGesture =
+        await tester.startGesture(tester.getCenter(primaryFinder));
+    await tester.pumpAndSettle();
 
-    for (final key in pressedKeys) {
-      final finder = find.byKey(Key(key));
-      await tester.startGesture(tester.getCenter(finder));
-      await tester.pumpAndSettle();
-    }
+    final secondaryFinder = find.byKey(const Key('pressed_secondary'));
+    final secondaryGesture =
+        await tester.startGesture(tester.getCenter(secondaryFinder));
+    await tester.pumpAndSettle();
 
     await screenMatchesGolden(tester, 'core_button_small');
 
-    for (final key in pressedKeys) {
-      final finder = find.byKey(Key(key));
-      final gesture = await tester.startGesture(tester.getCenter(finder));
-      await gesture.up();
-    }
+    await primaryGesture.up();
+    await secondaryGesture.up();
   });
 }
